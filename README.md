24c653c0cfb256b0aad564f767673fc531b189a7
Peter Murphy <pmurphy@newtown.(none)>
2013-07-04 12:44:13 +0100

    basic input bar

--- README.md
+++
@@ -1,6 +0,0 @@
-
-[FeedHenry](http://www.feedhenry.com "FeedHenry") Sample Application see:
-
-- [Developer Information](http://developer.feedhenry.com)
-
-- [API Documentation](http://docs.feedhenry.com)
--- cloud/config.js
+++
@@ -1,8 +0,0 @@
-var CONFIG = {
-  storeLimit: 0, // Limit on number of stores to show in search results. 0 = no limit
-  boundingBox: true, // Whether or not to apply a lat/lon bounding box restriction on store location queries
-  boundingBoxLat: 0.1,
-  boundingBoxLon: 0.1
-};
-
-exports.CONFIG = CONFIG;
